4b58f55e1145f754ec1d5e9619767c44bc3defdb
gardeneeel <chris040101@gmail.com>
2023-09-07 12:37:13 +0900

    week2 half adder

---
+++ week2/HA/HA.srcs/sources_1/new/HA.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/09/07 12:25:20
+// Design Name: 
+// Module Name: HA
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module HA(a, b, C, S);
+
+input a, b;
+output C, S;
+
+assign C = a & b;
+assign S = a ^ b;
+
+endmodule
