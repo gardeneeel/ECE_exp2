fae9c0fe0bdaebabd149adb59b923c54c2e605bd
gardeneeel <chris040101@gmail.com>
2023-11-22 18:12:49 +0900

    week12 LED expB

--- week12/LCD_cursor_expB/LCD_cursor_expB.srcs/sources_1/new/LCD_cursor.v
+++ week12/LCD_cursor_expB/LCD_cursor_expB.srcs/sources_1/new/LCD_cursor.v
@@ -25,13 +25,13 @@ module LCD_cursor(clk, rst, LCD_E, LCD_RS, LCD_RW, LCD_DATA, LED_out, number_btn
 input clk, rst;
 input [9:0] number_btn;
 input [1:0] control_btn;
-input LINE; // 0 : LINE1, 1 : LINE2
+input LINE;
 
 wire [9:0] number_btn_t;
 wire [1:0] control_btn_t;
 wire LINE1_t, LINE2_t;
 
-oneshot_universal #(.WIDTH(14)) O1(clk, rst, {number_btn[9:0], control_btn[1:0], LINE, ~LINE}, {number_btn_t[9:0], control_btn_t[1:0], LINE2_t, LINE1_t});
+oneshot_universal #(.WIDTH(14)) O1(clk, rst, {number_btn[9:0], control_btn[1:0], LINE, ~LINE}, {number_btn_t[9:0], control_btn_t[1:0], LINE1_t, LINE2_t});
 
 output LCD_E, LCD_RS, LCD_RW;
 output reg [7:0] LCD_DATA;
@@ -42,7 +42,7 @@ reg LCD_RS, LCD_RW;
 
 reg [7:0] cnt;
 
-reg [2:0] state;
+reg [3:0] state;
 parameter DELAY        = 4'b0000,
           FUNCTION_SET = 4'b0001,
           DISP_ONOFF   = 4'b0011,
@@ -110,11 +110,13 @@ always @(posedge clk or negedge rst) begin
                 else cnt <= cnt + 1;
             end
             SET_LINE1 : begin
+                LED_out <= 8'b0000_0011;
                 if(cnt == 30) state <= DELAY_T;
                 if(cnt >= 30) cnt <= 0;
                 else cnt <= cnt + 1;                
             end
             SET_LINE2 : begin
+                LED_out <= 8'b0000_0011;
                 if(cnt == 30) state <= DELAY_T;
                 if(cnt >= 30) cnt <= 0;
                 else cnt <= cnt + 1;             
@@ -134,7 +136,7 @@ always @(posedge clk or negedge rst) begin
             ENTRY_MODE :     
                 {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_0000_0110; // cursor increment, no display shift
             SET_ADDRESS :
-                {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1000_0000; // DD-RAM address set 0(LINE1)    
+                {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_0000_0010; // cursor at home  
             DELAY_T :
                 {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_0000_1111;    
             WRITE : begin
@@ -165,14 +167,10 @@ always @(posedge clk or negedge rst) begin
                 else {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_0000_1111;
             end
             SET_LINE1 : begin
-                if(~LINE) begin
-                    {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1000_0000; // DD-RAM address set 0(LINE1) 
-                end
+                {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1000_0000; // DD-RAM address set 0(LINE1)
             end
             SET_LINE2 : begin
-                if(LINE) begin
-                    {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1100_0000; // DD-RAM address set 40(LINE2)                 
-                end
+                {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1100_0000; // DD-RAM address set 40(LINE2)                 
             end
         endcase
     end
