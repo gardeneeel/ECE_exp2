8b897b814811e5a89554e91432f97e3bcc076fe4
gardeneeel <chris040101@gmail.com>
2023-11-22 16:43:11 +0900

    week12 LCD expC submodule

---
+++ week12/LCD_cursor_expC/LCD_cursor_expC.srcs/sources_1/new/oneshot_universal.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/11/19 01:55:07
+// Design Name: 
+// Module Name: oneshot_universal
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module oneshot_universal(clk, rst, btn, btn_trig);
+
+parameter WIDTH = 1;
+input clk, rst;
+input [WIDTH-1:0] btn;
+
+reg [WIDTH-1:0] btn_reg;
+output reg [WIDTH-1:0] btn_trig;
+
+always @(posedge clk or negedge rst) begin
+    if(!rst) begin
+        btn_reg <= {WIDTH{1'b0}};
+        btn_trig <= {WIDTH{1'b0}};
+    end
+    else begin
+        btn_reg <= btn;
+        btn_trig <= btn & ~btn_reg;
+    end
+end
+
+endmodule
